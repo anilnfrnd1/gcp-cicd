4bce22a6f72a08d01a4be3b84ea3c4f6bbc6b2bf
anilnfrnd1 <151373145+anilnfrnd1@users.noreply.github.com>
2026-01-09 17:59:32 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,12 +3,6 @@ variable "project_id" {
   type        = string
 }
 
-variable "region" {
-  description = "GCP region"
-  type        = string
-  default     = "us-central1"
-}
-
 variable "zone" {
   description = "GCP zone"
   type        = string
