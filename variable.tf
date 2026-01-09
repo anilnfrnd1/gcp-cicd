a92c55a9f8ed7a280209cec568fcbd74b418e348
anilnfrnd1 <151373145+anilnfrnd1@users.noreply.github.com>
2026-01-09 17:04:42 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,32 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "machine_type" {
+  description = "VM machine type"
+  type        = string
+  default     = "e2-medium"
+}
+
+variable "ssh_user" {
+  description = "SSH username"
+  type        = string
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key"
+  type        = string
+}
