7a146847bde3c14983cf1057266f6bcd1c8935db
anilnfrnd1 <151373145+anilnfrnd1@users.noreply.github.com>
2026-01-09 17:02:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
+resource "google_compute_instance" "vm" {
+  name         = "terraform-vm-cicd"
+  machine_type = var.machine_type
+  zone         = var.zone
 
+  boot_disk {
+    initialize_params {
+      image = "Ubuntu 25.10 Minimal"
+      size  = 10
+      type  = "pd-balanced"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {} # public IP
+  }
+}
