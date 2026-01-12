2c62901187963dc5ebc515d5a573efeacbe81c1d
anilnfrnd1 <151373145+anilnfrnd1@users.noreply.github.com>
2026-01-12 11:36:43 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ resource "google_compute_instance" "vm" {
   name         = "terraform-vm-cicd"
   machine_type = var.machine_type
   zone         = var.zone
-
+  allow_stopping_for_update = true
   boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2204-lts"
