5091837916eeb34967e13e8eb7ad5c8f9bf187d8
anilnfrnd1 <151373145+anilnfrnd1@users.noreply.github.com>
2026-01-29 12:41:37 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,24 @@
-
 resource "google_storage_bucket" "my_first_bucket" {
-  name     = "first4562535142"
-  location = "var.region"
+  name          = "my-unique-bucket-name-2026" # Must be globally unique
+  location      = "US-CENTRAL1"               # Use lowercase for regions, uppercase for multi-regions
+  force_destroy = true                        # Allows 'terraform destroy' to delete non-empty buckets
+
+  # Security: Prevent accidental public exposure
+  public_access_prevention = "enforced"
+
+  # Reliability: Keep history of your files
+  versioning {
+    enabled = true
+  }
+
+  # Cost Management: Move files to Nearline storage after 30 days
+  lifecycle_rule {
+    condition {
+      age = 30
+    }
+    action {
+      type = "set_storage_class"
+      storage_class = "NEARLINE"
+    }
+  }
 }
